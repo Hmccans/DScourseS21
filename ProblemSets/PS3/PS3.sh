0893b73f715c8d4a194acdb6afccc8d2da8a0b0e
Hunter McCans <78052221+Hmccans@users.noreply.github.com>
2021-02-18 09:44:42 -0600

    editing text

---
+++ ProblemSets/PS3/PS3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#download the data
+wget http://spatialkeydocs.s3.amazonaws.com/FL_insurance_sample.csv.zip
+
+#Uncompress
+unzip FL_insurance_sample.csv.zip
+
+#delete extraneous files
+rm -rf __MACOSX
+rm -f FL_insurance_sample.csv.zip
+
+
+
+#computer frying time
+head -5 FL_insurance_sample.csv
+wc -l FL_insurance_sample.csv
+
+
+#convert
+dos2unix -c mac FL_insurance_sample.csv
+
+wc -l FL_insurance_sample.csv
